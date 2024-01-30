060467dd9a9ea8ce99101da9758bc2a6a788095c
Elizabeth Marston <315987+elmarsto@users.noreply.github.com>
2024-01-30 00:03:16 -0800

    nice

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  inputs = {
+    flake-compat.url = "https://flakehub.com/f/edolstra/flake-compat/1.tar.gz";
+    flake-utils.url = "github:numtide/flake-utils";
+    gitignore = {
+      url = "github:hercules-ci/gitignore.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixpkgs.url = "github:NixOS/nixpkgs";
+  };
+  outputs =
+    { flake-utils, gitignore, nixpkgs }: flake-utils.lib.eachDefaultSystem (system:
+      let
+        app = p: { program = "${p}"; type = "app"; };
+        pkgs = import nixpkgs { inherit system; };
+        src = gitignore.lib.gitignoreSource ./.;
+      in
+      {
+        apps = { };
+        devShells = { };
+        packages = { };
+      }
+    );
+}
